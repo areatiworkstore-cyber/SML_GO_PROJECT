cb87bf5e86f640d0d44ab587f6c4301323fed49c
areatiworkstore-cyber <areatiworkstore@gmail.com>
2026-06-08 10:19:45 -0500

    Fix: Correccion con el import de RoleUserResponse

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from typing import Optional, List
 from pydantic import BaseModel, EmailStr
 from app.schemas.master_data import DocumentTypeResponse
-from app.schemas.user import RoleUserResponse
 
 # --- SCHEMA DE USER ---
 class UserBase(BaseModel):
